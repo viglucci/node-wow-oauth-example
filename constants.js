72fdb87c9508430a8fbe575a3bd17ed30219b899
Kevin Viglucci <kviglucci@gmail.com>
2020-09-05 20:37:46 -0500

    move font files to resources/fonts

--- constants.js
+++ constants.js
@@ -8,6 +8,6 @@ module.exports = {
     BACKGROUND_IMAGE_ALLIANCE_PATH: path.resolve('./images/background-0.png'),
     BACKGROUND_IMAGE_HORDE_PATH: path.resolve('./images/background-1.png'),
     BACKGROUND_IMAGE_NEUTRAL_PATH: path.resolve('./images/background-2.png'),
-    FONT_MERRIWEATHER_BOLD_PATH: path.resolve('./fonts/merriweather/Merriweather-Bold.ttf'),
-    FONT_MERRIWEATHER_REGULAR_PATH: path.resolve('./fonts/merriweather/Merriweather-Regular.ttf'),
+    FONT_MERRIWEATHER_BOLD_PATH: path.resolve('./resources/fonts/merriweather/Merriweather-Bold.ttf'),
+    FONT_MERRIWEATHER_REGULAR_PATH: path.resolve('./resources/fonts/merriweather/Merriweather-Regular.ttf'),
 };
